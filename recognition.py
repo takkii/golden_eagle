6e2780428cce37a84ca014dc89a0d2a2d8750f4b
Ｔ・Ｋ <karuma.reason@gmail.com>
2026-01-10 10:20:13 +0900

    Update.

--- recognition.py
+++ recognition.py
@@ -56,20 +56,15 @@ while True:
         # Convert the image to COLOR_BGR2RGB color (which face_recognition uses)
         rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
 
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
+        # The default is "hog" / other select "cnn"
+        face_locations = face_recognition.face_locations(rgb_small_frame, model='cnn')
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            # tolerance = 0.4, default value.
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.4)
             name = "Unknown"
-
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
 
             # Or instead, use the known face with the smallest distance to the new face
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
