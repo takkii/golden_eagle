65a4fe5436c7ec1f28c0ce7f29e1bd65c892802f
Ｔ・Ｋ <karuma.reason@gmail.com>
2026-01-25 07:28:39 +0900

    Create & Delete.

--- build/lib/golden_eagle/__init__.py
+++
@@ -1,9 +0,0 @@
-from importlib.metadata import version
-
-from .main import compare_before_after
-
-# function call
-__all__ = ['compare_before_after']
-
-# version
-__version__ = version(__package__)
