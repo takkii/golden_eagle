1481651bec61c9479b0080a04c3611d0e6556764
Ｔ・Ｋ <karuma.reason@gmail.com>
2026-01-25 07:47:49 +0900

    Update.

--- golden_eagle/__init__.py
+++ golden_eagle/__init__.py
@@ -2,9 +2,10 @@ from importlib.metadata import version
 
 from .main import compare_before_after
 from .main import security
+from .main import recognition
 
 # function call
-__all__ = ['compare_before_after', 'security']
+__all__ = ['compare_before_after', 'security', 'recognition']
 
 # version
 __version__ = version(__package__)
